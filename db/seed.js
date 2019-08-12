b29854c0a260c1bbe728b1c7ef73b138ce8d7dd7
Jamie Miller <miller0328@gmail.com>
2019-08-12 18:55:16 -0400

    index.js file

---
+++ db/seed.js
@@ -0,0 +1,6 @@
+const Genre = require('../models/Genre');
+const Movie = require('../models/Movie');
+const Person = require('../models/Person');
+
+let movie = new Movie ({name: 'Test'})
+
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const parser = require("body-parser");
+const app = express();
+
+app.use(parser.json());
+
+app.use("/genre", require("./routes/genre"));
+app.use("/movie", require("./routes/movie"));
+app.use("/person", require("./routes/person"));
+
+app.listen(4000, () => console.log("server running on 4000"));
