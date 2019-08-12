6f85aabb7e346b9b5f70e5e4a440ad2f2e950377
Jamie Miller <miller0328@gmail.com>
2019-08-12 18:42:35 -0400

    person routes

---
+++ routes/person.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const personController = require('../controllers/person');
+
+router.get('/', personController.index)
+router.get('/name/:name', personController.show)
