70fea52034ebe82a4b6f2736e1487d52ce30d789
Jamie Miller <miller0328@gmail.com>
2019-08-21 22:16:54 -0400

    editing index

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ app.use("/genre", require("./routes/genre"));
 app.use("/movie", require("./routes/movie"));
 app.use("/person", require("./routes/person"));
 
-app.listen(4000, () => console.log("server running on 4000"));
+
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+    console.log(`PORT: ${app.get("port")}`);
+});
+
+// app.listen(4000, () => console.log("server running on 4000"));
