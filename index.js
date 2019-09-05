82959d27ea5a04697e21313dff7e14725917618c
Jamie Miller <miller0328@gmail.com>
2019-09-05 19:57:36 -0400

    mj edits

--- index.js
+++ index.js
@@ -4,15 +4,11 @@ const app = express();
 
 app.use(parser.json());
 
-app.use("/movie", require("./routes/movie"));
+app.use("/", require("./routes/movie"));
 app.use("/review", require("./routes/review"));
 app.use('/rating', require("./routes/rating"));
 
-
 app.set("port", process.env.PORT || 8080);
 
-app.listen(app.get("port"), () => {
-    console.log(`PORT: ${app.get("port")}`);
-});
-
-// app.listen(4000, () => console.log("server running on 4000"));
+app.listen(app.get("port"), () => 
+ console.log('✅ PORT: ${app.get("port")} 🌟')
