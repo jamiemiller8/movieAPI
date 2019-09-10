97e9295f259f91fcfba4260c117630850428e12a
Jamie Miller <miller0328@gmail.com>
2019-09-10 11:37:14 -0400

    Switched back ports

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ app.use("/", require("./routes/movie"));
 app.use("/review", require("./routes/review"));
 app.use('/rating', require("./routes/rating"));
 
-// app.set("port", process.env.PORT || 8080);
+app.set("port", process.env.PORT || 8080);
 
-// app.listen(app.get("port"), () => 
-//  console.log(`✅ PORT: ${app.get("port")} 🌟`))
+app.listen(app.get("port"), () => 
+ console.log(`✅ PORT: ${app.get("port")} 🌟`))
 
-app.listen(3000, () => console.log('listening on port 3000'))
